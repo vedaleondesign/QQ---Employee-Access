262ccb3248aa664cea644e68cfa9fb1b677d1a74
Dave King <dave.king@vedaleon.com>
2018-04-11 10:29:55 +1000

    Root 1.0

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$( ".create-account" ).click(function() {
+  $( "#create" ).show();
+  $( "#enter" ).hide();
+});
+$( ".login-account" ).click(function() {
+  $( "#create" ).hide();
+  $( "#enter" ).show();
+});
+
+
+//
